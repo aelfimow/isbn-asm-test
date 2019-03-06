10315938240be331a211449843a2225fb752a391
Alexander Elfimow <my.free.mail@gmx.de>
2019-03-06 23:07:40 +0100

    isbn-13: adjusted tests

--- tests/test-isbn-13.cpp
+++ tests/test-isbn-13.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <gtest/gtest.h>
 #include <isbn-13.h>
 
@@ -11,7 +12,6 @@ static uint8_t golden_isb13(const uint8_t *numbers)
     sum1 += numbers[6];
     sum1 += numbers[8];
     sum1 += numbers[10];
-    sum1 += numbers[12];
 
     size_t sum2 = 0;
 
@@ -24,9 +24,9 @@ static uint8_t golden_isb13(const uint8_t *numbers)
 
     size_t sum = sum1 + (3 * sum2);
 
-    size_t rest1 = sum % 10;
+    size_t r = sum % 10;
 
-    size_t result = (10 - rest1) % 10;
+    size_t result = (10 - r) % 10;
 
     return static_cast<uint8_t>(result);
 }
@@ -36,10 +36,10 @@ TEST(ISBN13, A)
     const uint8_t numbers[] = { 9, 7, 8, 3, 7, 6, 5, 7, 2, 7, 8, 1 };
 
     const auto golden_result = golden_isb13(numbers);
-    EXPECT_EQ(golden_result, 2);
+    EXPECT_EQ(golden_result, 8);
 
     const auto result = isbn13(numbers);
-    EXPECT_EQ(result, 2);
+    EXPECT_EQ(result, 8);
 }
 
 TEST(ISBN13, B)
